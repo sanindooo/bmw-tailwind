beac3b844333b57acf5fe177e48df7e21ee03011
Stephen Anindo <sanindo@hotmail.co.uk>
2021-11-11 13:41:24 +0000

    init commit

---
+++ public/app.js
@@ -0,0 +1,72 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// mobile navbar
+const burger = document.querySelector("#burger");
+const closeIcon = document.querySelector("#close");
+const mobileBtn = document.querySelector("#mobileBtn");
+const navMenu = document.querySelector("#nav");
+
+mobileBtn.addEventListener('click', () => {
+    burger.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+    navMenu.classList.toggle('hidden');
+});
+
+// sldier 
+var flkty = new Flickity( '.main-carousel', {
+    cellAlign: 'center',
+    contain: true,
+    fade: true,
+    autoPlay: 6000,
+    wrapAround: true, 
+    freeScroll: true,
+    pageDots: false,
+    prevNextButtons: false,
+});
+
+// animations 
+
+// hero animation on load 
+let tl =gsap.timeline();
+
+tl
+.from("#hero-bg", { opacity: 0, delay: 0.5, duration: 2, scale: 0.8 })
+.from("#fade-in-1", { opacity: 0, duration: 1 }, "-=1")
+
+// ___________________________________
+
+// Stagger highlight section 
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#highlights",      
+    }
+});
+
+tl2
+.from("#highlights a", { start: "bottom center", opacity: 0, y: 50, stagger: 0.1 })
+
+
+// ___________________________________
+
+// Stagger specification section 
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "specs",    
+        start: "top top"  
+    }
+});
+
+tl3
+.from("#slide-right", { start: "bottom center", x:-100, opacity:0, duration: 0.5 })
+.from("#specs li", { start: "bottom center", opacity: 0, y: 50, stagger: 0.1})
+
+// ___________________________________
+    
+// heading and text animations 
+gsap.from("#fade-up-1", { scrollTrigger:"#fade-up-1", start: "center bottom", y:50, opacity:0, duration: 0.5 })
+gsap.from("#fade-up-2", { scrollTrigger:"#fade-up-2", start: "center bottom", y:50, opacity:0, duration: 0.5 })
+gsap.from("#fade-in-2", { scrollTrigger:"#fade-in-2", start: "center center", opacity:0, duration: 1, delay: 0.3 })
+gsap.from("#fade-in-3", { scrollTrigger:"#fade-in-3", start: "center center", opacity:0, duration: 1, delay: 0.3 })
+gsap.from("#fade-in-4", { scrollTrigger:"#fade-in-4", start: "center center", opacity:0, duration: 1, delay: 0.3 })
+
+// ___________________________________
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem'
+        },
+        extend: {
+            backgroundImage: {
+             'hero': "url('../img/dark-bmw.jpg')",
+             'image-charging': "url('../img/bmw-i4-m50-onepager-gallery-impressions-wallpaper-01.jpg')",
+             'image-touchscreen': "url('../img/bmw-i4-m50-onepager-gallery-impressions-thumbnail-06.jpg.asset.1625831625838.jpg')",
+             'image-breaks': "url('../img/image.1626181544798.jpg')",
+            },
+            fontFamily: {
+                'heading': ['"Orbitron"', 'sans-serif'],
+                'paragraph': ['"Roboto"', 'sans-serif'],
+            }
+        },
+    }
+  }
