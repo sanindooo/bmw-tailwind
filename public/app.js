ace005556b5b86117d407dac1206ae865809dc66
Stephen Anindo <sanindo@hotmail.co.uk>
2021-11-11 15:30:50 +0000

    fixed animation timings, add footer animation

--- public/app.js
+++ public/app.js
@@ -32,6 +32,7 @@ let tl =gsap.timeline();
 tl
 .from("#hero-bg", { opacity: 0, delay: 0.5, duration: 2, scale: 0.8 })
 .from("#fade-in-1", { opacity: 0, duration: 1 }, "-=1")
+.from("#fade-down", { y: -100, opacity: 0, duration: 0.5 })
 
 // ___________________________________
 
@@ -39,12 +40,12 @@ tl
 let tl2 = gsap.timeline({
     scrollTrigger: {
         trigger: "#highlights",   
-        start: "75% 100%"   
+        start: "20% 100%"   
     }
 });
 
 tl2
-.from("#highlights a", { opacity: 0, y: 50, stagger: 0.1 })
+.from("#highlights a", { opacity: 0, y: 50, stagger: 0.5 })
 
 
 // ___________________________________
@@ -52,22 +53,35 @@ tl2
 // Stagger specification section 
 let tl3 = gsap.timeline({
     scrollTrigger: {
-        trigger: "specs",    
-        start: "75% 50%"  
+        trigger: "techspecs",    
+        start: "80% 100%"  
     }
 });
 
 tl3
-.from("#slide-right", { x:-100, opacity:0, duration: 0.5 })
-.from("#specs li", { opacity: 0, y: 50, stagger: 0.1})
+.from("#slide-right", { x:-100, opacity:0, duration: 1 })
+.from("#specs li", { opacity: 0, y: 50, stagger: 0.2})
+
+// ___________________________________
+
+// Stagger footer 
+let tl5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#footer",    
+        start: "top 90%"  
+    }
+});
+
+tl5
+.from(".stagger-links a", { opacity: 0, y: 50, stagger: 0.1})
 
 // ___________________________________
     
 // heading and text animations 
-gsap.from("#fade-up-1", { scrollTrigger:"#fade-up-1", start: "75% 100%", y:50, opacity:0, duration: 0.5 })
-gsap.from("#fade-up-2", { scrollTrigger:"#fade-up-2", start: "75% 100%", y:50, opacity:0, duration: 0.5 })
-gsap.from("#fade-in-2", { scrollTrigger:"#fade-in-2", start: "75% 100%", opacity:0, duration: 1, delay: 0.3 })
-gsap.from("#fade-in-3", { scrollTrigger:"#fade-in-3", start: "75% 100%", opacity:0, duration: 1, delay: 0.3 })
-gsap.from("#fade-in-4", { scrollTrigger:"#fade-in-4", start: "75% 100%", opacity:0, duration: 1, delay: 0.3 })
+gsap.from("#fade-up-1", { scrollTrigger:"#fade-up-1", start: "center center", y:50, opacity:0, duration: 1 })
+gsap.from("#fade-up-2", { scrollTrigger:"#fade-up-2", start: "center center", y:50, opacity:0, duration: 1 })
+gsap.from("#fade-in-2", { scrollTrigger:"#fade-in-2", start: "center center", opacity:0, duration: 1.5, delay: 0.3 })
+gsap.from("#fade-in-3", { scrollTrigger:"#fade-in-3", start: "center center", opacity:0, duration: 1.5, delay: 0.3 })
+gsap.from("#fade-in-4", { scrollTrigger:"#fade-in-4", start: "center center", opacity:0, duration: 1.5, delay: 0.3 })
 
 // ___________________________________
